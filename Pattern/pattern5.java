56e93a5e251e75065ef7ab23fc4fb987bab76d1d
Tarun Tomar <taruntomar022@icloud.com>
2020-04-11 21:04:29 +0530

    Diamond shaped pattern

--- Pattern/pattern5.java
+++ Pattern/pattern5.java
@@ -5,7 +5,6 @@ import java.util.*;
                public static void main(String[] args) {
                    Scanner scn = new Scanner(System.in);
                
-                    // write ur code here
                     int n = scn.nextInt();
                     // int mid = (n/2)+1;
                     // int istar = 1;
