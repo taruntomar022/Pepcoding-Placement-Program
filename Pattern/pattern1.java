011964c32e765b4be2e2459169fe894991295b5f
Tarun Tomar <taruntomar022@icloud.com>
2020-04-11 20:55:03 +0530

    Triangle pattern (L shaped)

--- Pattern/pattern1.java
+++ Pattern/pattern1.java
@@ -7,7 +7,7 @@ import java.util.*;
        public static void main(String[] args) {
            Scanner scn = new Scanner(System.in);
        
-            // write ur code here
+            
             int n = scn.nextInt();
             for(int i =0;i<n;i++){
                 for(int j =0;j<=i;j++){
