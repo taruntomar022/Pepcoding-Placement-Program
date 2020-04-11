68f58e2bdad6f9b2fde52c7a9e26735ea6389781
Tarun Tomar <taruntomar022@icloud.com>
2020-04-11 20:58:20 +0530

    Upper Triangle pattern

--- Pattern/pattern2.java
+++ Pattern/pattern2.java
@@ -7,7 +7,7 @@ import java.util.*;
       public static void main(String[] args) {
           Scanner scn = new Scanner(System.in);
       
-           // write ur code here
+           
            int n = scn.nextInt();
            int nrow = n;
            int irow = 1;
