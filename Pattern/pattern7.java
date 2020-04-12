346814064279f9f300e65cd508b62dac7b174f13
Tarun Tomar <taruntomar022@icloud.com>
2020-04-12 22:11:00 +0530

    Printing left diagonal

--- Pattern/pattern7.java
+++ Pattern/pattern7.java
@@ -5,7 +5,6 @@ import java.util.*;
      public static void main(String[] args) {
          Scanner scn = new Scanner(System.in);
      
-          // write ur code here
           int n = scn.nextInt();
           for(int i =0;i<n;i++){
               for(int j=0;j<n;j++){
