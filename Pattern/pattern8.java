1aee6fcae73523e4398328ed65ab09a5424f2ab5
Tarun Tomar <taruntomar022@icloud.com>
2020-04-12 22:11:55 +0530

    Printing right diagonal

--- Pattern/pattern8.java
+++ Pattern/pattern8.java
@@ -5,7 +5,6 @@ import java.util.*;
       public static void main(String[] args) {
           Scanner scn = new Scanner(System.in);
       
-           // write ur code here
            int n = scn.nextInt();
           for(int i =1;i<=n;i++){
               for(int j=1;j<=n;j++){
