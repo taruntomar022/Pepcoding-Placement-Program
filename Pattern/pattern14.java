f7c7208154d1f2eb3571b3ab5bb85e00ef471f60
Tarun Tomar <taruntomar022@icloud.com>
2020-04-13 22:49:11 +0530

    Table of a number

--- Pattern/pattern14.java
+++ Pattern/pattern14.java
@@ -4,8 +4,7 @@ import java.util.*;
       
       public static void main(String[] args) {
           Scanner scn = new Scanner(System.in);
-      
-           // write ur code here
+     
            int n = scn.nextInt();
            for(int i=1;i<=10;i++){
                int p = n*i;
