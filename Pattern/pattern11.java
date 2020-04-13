6acb0114833bd2bcd56ccc07c107c91b63ff956c
Tarun Tomar <taruntomar022@icloud.com>
2020-04-13 22:46:58 +0530

    Triangle pattern printing numbers

--- Pattern/pattern11.java
+++ Pattern/pattern11.java
@@ -5,7 +5,6 @@ import java.util.*;
      public static void main(String[] args) {
          Scanner scn = new Scanner(System.in);
      
-          // write ur code here
           int a =1;
           int n = scn.nextInt();
           for(int i=0;i<n;i++){
