6096ccae0a4a81112cc0161a84da418e99fe2f0a
Tarun Tomar <taruntomar022@icloud.com>
2020-04-14 12:39:45 +0530

    Swastik Pattern

--- Pattern/pattern19.java
+++ Pattern/pattern19.java
@@ -7,7 +7,6 @@ import java.util.*;
       public static void main(String[] args) {
           Scanner scn = new Scanner(System.in);
       
-           // write ur code here
            int n = scn.nextInt();
            int m = (n/2)+1;
            for(int i=1;i<=n;i++){
