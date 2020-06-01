8a036be26feee50c836eb7740b76b6d234c0f937
Tarun Tomar <taruntomar022@icloud.com>
2020-06-01 22:22:09 +0530

    Print fibonnaci series

--- Lecture1(Basic Programming)/fibonacciSeries.java
+++ Lecture1(Basic Programming)/fibonacciSeries.java
@@ -12,7 +12,7 @@ import java.util.*;
       int n = scn.nextInt();
       int i = 0,j=1,sum;
       for(int k=1;k<=n;k++){
-          if(i<10){
+          if(i<10){ 
         System.out.println(i);
           sum = i + j;
           i=j;
