528aff150821c98424d4f416781847c021e5761c
Tarun Tomar <taruntomar022@icloud.com>
2020-06-01 22:11:45 +0530

    Print Z Pattern

--- Lecture1(Basic Programming)/printZ.java
+++ Lecture1(Basic Programming)/printZ.java
@@ -2,7 +2,7 @@ import java.util.*;
           
           public class Main {
           
-              public static void main(String[] args) {
+              public static void main(String[] args) { 
          	         System.out.println("*****");
          	         System.out.println("   *");
          	         System.out.println("  *");
