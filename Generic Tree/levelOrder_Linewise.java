1c9d137178d889cfeb2987d1979ff54fc8bd52c9
Tarun Tomar <taruntomar022@icloud.com>
2020-06-14 08:23:03 +0530

    Levelorder linewise with different approaches

--- Generic Tree/levelOrder_Linewise.java
+++ Generic Tree/levelOrder_Linewise.java
@@ -5,6 +5,21 @@ public class Main {
   private static class Node {
     int data;
     ArrayList<Node> children = new ArrayList<>();
+    // Node(){
+    //     this.data = data;
+    // }
+    // Node(int val){
+    //     this.data = val;
+    // }
+  }
+  
+  private static class Pair{
+     Node node;
+     int level;
+     Pair(Node node,int level){
+         this.node = node;
+         this.level= level;
+     }
   }
 
   public static void display(Node node) {
@@ -90,30 +105,101 @@ public class Main {
 
     System.out.println("Node Post " + node.data);
   }
+//---------------Using main queue and child queue----------------------
+//   public static void levelOrderLinewise(Node node){
+//     // write your code here
+//     Queue<Node> childQue = new ArrayDeque();
+//     Queue<Node> mainQue = new ArrayDeque();
+//     mainQue.add(node);
+//     while(mainQue.size()>0){
+//         // remove
+//         Node temp = mainQue.remove();
+        
+//         // print
+//         System.out.print(temp.data+" ");
+//         // add in child queue
+//         for(Node child:temp.children){
+//             childQue.add(child);
+//         }
+        
+//         if(mainQue.size()==0){
+//             System.out.println();
+//             mainQue = childQue;
+//             childQue = new ArrayDeque();
+//         }
+//     }
+//   }
 
-  public static void levelOrderLinewise(Node node){
-    // write your code here
-    Queue<Node> childQue = new ArrayDeque();
-    Queue<Node> mainQue = new ArrayDeque();
-    mainQue.add(node);
-    while(mainQue.size()>0){
-        // remove
-        Node temp = mainQue.remove();
-        
-        // print
-        System.out.print(temp.data+" ");
-        // add in child queue
-        for(Node child:temp.children){
-            childQue.add(child);
-        }
-        
-        if(mainQue.size()==0){
-            System.out.println();
-            mainQue = childQue;
-            childQue = new ArrayDeque();
+// --------------adding null to the queue when level completes--------------------
+//   public static void levelOrderLinewise(Node node){
+//       Queue<Node> que = new ArrayDeque();
+//       que.add(node);
+//       que.add(new Node(Integer.MIN_VALUE));
+//       while(que.size()>0){
+//           // remove
+//           Node temp = que.remove();
+//           if(temp.data == Integer.MIN_VALUE){
+//               // add a new line
+//               System.out.println();
+//               if(que.size()>0){
+//                 que.add(new Node(Integer.MIN_VALUE));
+//               }
+//           }else{
+//               // print
+//               System.out.print(temp.data+" ");
+//               // add child in queue
+//               for(Node child: temp.children){
+//                   que.add(child);
+//               }
+//           }
+//       }
+//   }
+
+// --------------using count variable, approach----------------------
+    //  public static void levelOrderLinewise(Node node){
+    //      Queue<Node> que = new ArrayDeque();
+    //      que.add(node);
+    //      while(que.size()>0){
+    //         int count = que.size();
+    //         for(int i=0;i<count;i++){
+    //             //remove
+    //             node = que.remove();
+    //             //print
+    //             System.out.print(node.data+" ");
+    //             // add
+    //             for(Node child:node.children){
+    //                 que.add(child);
+    //             }
+    //         } 
+    //         System.out.println();
+    //      }
+         
+         
+    //  }
+
+// -----------------using level-maintain approach-----------------------
+
+    public static void levelOrderLinewise(Node node){
+        Queue<Pair> que = new ArrayDeque<>();
+        que.add(new Pair(node,1));
+        int level = 1;
+        while(que.size()>0){
+            //remove
+            Pair remP = que.remove();
+            Node temp = remP.node;
+            int pl = remP.level;
+            if(remP.level > level){
+                level = remP.level;
+                System.out.println();
+            }
+            //print
+            System.out.print(temp.data+" ");
+            //add
+            for(Node child: temp.children){
+                que.add(new Pair(child,level+1));
+            }
         }
     }
-  }
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
