d7f9526ca98cae552f53ffd01a1e3de7eec2821a
Tarun Tomar <taruntomar022@icloud.com>
2020-06-20 23:01:58 +0530

    Is Binary tree balanced

--- Binary Tree/isBalanced.java
+++ Binary Tree/isBalanced.java
@@ -7,7 +7,7 @@ public class Main {
     Node left;
     Node right;
 
-    Node(int data, Node left, Node right) {
+    Node(int data, Node left, Node right) { 
       this.data = data;
       this.left = left;
       this.right = right;
