38f6d015497317ca8b225f35b430631d65ef1363
Tarun Tomar <taruntomar022@icloud.com>
2020-06-21 22:32:57 +0530

    Sort K sorted array

--- Priority Queue/sort_k_sorted_array.java
+++ Priority Queue/sort_k_sorted_array.java
@@ -5,7 +5,7 @@ public class Main {
 
    public static void main(String[] args) throws Exception {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int n = Integer.parseInt(br.readLine());
+      int n = Integer.parseInt(br.readLine()); 
       int[] arr = new int[n];
 
       for (int i = 0; i < n; i++) {
